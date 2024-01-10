e994f4b4adf95dfa62aece5bdcbab802e08b4f9c
maisaabi <maisaabi47@gmail.com>
2024-01-10 23:12:32 +0700

    meet 6

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Order;
+use App\Models\OrderItem;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Category::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            UserSeeder::class,
+            ProductSeeder::class,
+            OrderSeeder::class,
+            OrderItemSeeder::class,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +16,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return view('pages.blank-page', ['type_menu' => '']);
-   });
+    return view('auth.login', ['type_menu' => '']);
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => '']);
+    })->name('home')->middleware('can:dashboard');
+    Route::get('profile-edit', function () {
+        return view('pages.profile', ['type_menu' => '']);
+    })->name('profile.edit');
+
+    Route::resource('user', UserController::class);
+});
+
+
+// Route::get('/login', function () {
+//     // return view('welcome');
+//     return view('auth.login');
+// });
+
+// Route::get('/register', function () {
+//     return view('auth.register');
+// });
+
+// Route::get('/reset', function () {
+//     return view('auth.reset');
+// });
+
+// Route::get('/forgot', function () {
+//     return view('auth.forgot');
+// });
+
+// Route::get('/verify', function () {
+//     return view('auth.verify');
+// });
